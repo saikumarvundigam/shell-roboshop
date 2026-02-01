9fdb22a4b8535c8da494b7048ea42c80b7111add
saikumar <saikumar.vundigam@gmail.com>
2026-02-01 14:31:17 +0530

    Recordsupdate

--- roboshop.sh
+++ roboshop.sh
@@ -17,7 +17,7 @@ do
         --query 'Instances[0].InstanceId' \
         --output text )
 
-        if [ $instance == "frontend" ]; then
+        if [ "$instance" = "frontend" ]; then
            IP=$(
                 aws ec2 describe-instances \
                 --instance-ids $INSTANCE_ID \
