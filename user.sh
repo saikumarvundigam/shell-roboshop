a23479cd739fed4aba5d931cd690c357ac692f83
saikumar <saikumar.vundigam@gmail.com>
2026-02-03 02:50:46 +0530

    Recordsupdate

--- user.sh
+++ user.sh
@@ -27,3 +27,68 @@ else
 echo "$2 success" | tee -a  $LOG_FILE
 fi
 }
+
+
+dnf module disable nodejs -y &>> $LOG_FILE
+VALIDATE $? "Disablinig NodeJS default version"
+
+dnf module enable nodejs:20 -y &>> $LOG_FILE
+VALIDATE $? "Enabling od Node JS 20 version is"
+
+dnf install nodejs -y &>> $LOG_FILE
+VALIDATE $? "NODE JS installation is"
+
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "Roboshop user creation is"
+else
+echo "Roboshp user already exists. Skipping...!!!"
+fi
+
+mkdir -p /app
+VALIDATE $? "Directory creation is"
+
+cd /app
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOG_FILE
+VALIDATE $? "Downloading catalogue code"
+
+cp /tmp/catalogue.zip /app
+VALIDATE $? "Copying Catalogue file to app is"
+
+unzip catalogue.zip &>>$LOG_FILE
+VALIDATE $? "Uzip catalogue code"
+
+npm install  &>>$LOG_FILE
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "Creation of Service is"
+
+systemctl daemon-reload
+systemctl enable catalogue  &>>$LOG_FILE
+systemctl start catalogue
+VALIDATE $? "Starting and enabling catalogue"
+
+cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Repo update is"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+
+INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Loading products"
+else
+    echo -e "Products already loaded ... $Y SKIPPING $N"
+fi
+
+systemctl restart catalogue
+VALIDATE $? "Restarting catalogue"
