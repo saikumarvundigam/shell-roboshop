2e49da64537ca087a84266166260ace18701c740
saikumar <saikumar.vundigam@gmail.com>
2026-02-03 02:30:55 +0530

    Recordsupdate

---
+++ redis.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+B="\e[33m"
+Y="\e[34m"
+N="\e[0m"
+LOG_FOLDER="/var/log/roboshop"
+LOG_FILE="$LOG_FOLDER"/$0.log
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+echo "$R Please run the script using root user $N" | tee -a $LOG_FILE
+exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE()
+{
+if [ $1 -ne 0 ]; then
+echo "$2 failed." | tee -a  $LOG_FILE
+else
+echo "$2 success" | tee -a  $LOG_FILE
+fi
+}
+
+dnf module disable redis -y
+dnf module enable redis:7 -y &>> $LOG_FILE
+VALIDATE $? "Enabling Redi 7 is"
+
+dnf install redis -y  &>> $LOG_FILE
+VALIDATE $? "Redis Installation is"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode c /pr otected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing Remote connections and disabling protected-mode is"
+
+systemctl enable redis &>> $LOG_FILE
+VALIDATE $? "Enabling REDIS is"
+
+systemctl start redis &>> $LOG_FILE
+VALIDATE $? "Starting Redis Service is"
