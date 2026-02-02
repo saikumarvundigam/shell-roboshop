3c9adc15d9175c30bc246d8d964874b241865721
saikumar <saikumar.vundigam@gmail.com>
2026-02-03 04:51:21 +0530

    Recordsupdate

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -34,11 +34,10 @@ VALIDATE  $? "Adding RabbitMQ Repo is"
 dnf install rabbitmq-server -y &>> $LOG_FILE
 VALIDATE $? "RabbitMQ installation  is"
 
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl enable rabbitmq-server &>> $LOG_FILE
+systemctl start rabbitmq-server &>> $LOG_FILE
 VALIDATE $? "Enabling and Staring of RabbitMQ service is"
 
 rabbitmqctl add_user roboshop roboshop123 &>> $LOG_FILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOG_FILE
-VALIDATE $? "Adding User and Set Permisssions are
-
+VALIDATE $? "Adding User and Set Permisssions are"
